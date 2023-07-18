66a8578aca6b078762aa97a96e2095966d3b33e1
Pareekshith TG <pareekshithtg@gmail.com>
2023-07-18 13:11:28 +0530

    back comment

---
+++ Employee_Backend/empo/src/test/java/employeebackend/empo/EmpoApplicationTests.java
@@ -0,0 +1,13 @@
+package employeebackend.empo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
